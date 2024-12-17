ad75ef5d3afba0b268df09b498ea10b2a6474900
JosinaldoRodrigues <josinaldor869@gmail.com>
2024-12-17 03:47:41 -0300

    updd

--- config.py
+++ config.py
@@ -3,7 +3,9 @@ from flask import Flask, redirect, render_template, request, jsonify
 db = SQLAlchemy()
 import os
 
+
 class Config:
     SQLALCHEMY_DATABASE_URI =  os.environ.get('BANCO_SPOTIFY') 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = 'Sempreemforma'
+
+  
